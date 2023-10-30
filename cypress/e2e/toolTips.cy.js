5da3a5a54c57a7d820c1810e477bf5a3a3e4f681
Tatiana Naumova <tatiananaumova5@gmail.com>
2023-10-30 15:55:21 -0400

    add toolTips.cy.js.cy.js

---
+++ cypress/e2e/toolTips.cy.js
@@ -0,0 +1,16 @@
+describe('Testing Tool Tips page', () => {
+    it('Testing tool tips button', ()=> {
+        cy.visit('https://demoqa.com/tool-tips');
+
+        cy.get('#toolTipButton').trigger('mouseover');
+        cy.get('.tooltip-inner').should('have.text', 'You hovered over the Button');
+    })
+
+    it('Testing tool tips field', ()=> {
+        cy.visit('https://demoqa.com/tool-tips');
+
+        cy.get('#toolTipTextField').trigger('mouseover');
+        cy.get('.tooltip-inner').should('have.text', 'You hovered over the text field');
+    })
+
+})
